575d0a42795a1e73a2931417e603c33a5129836b
damsu <damorizur@gmail.com>
2016-10-04 14:26:36 +0300

    Add files via upload

--- posts.js
+++ posts.js
@@ -1,4 +1,4 @@
-var data = require('../data');
+var data = require('./data.js');
 var express = require('express');
 var router = express.Router();
 
@@ -7,7 +7,7 @@ var router = express.Router();
 //
 
 function getAllPosts(req, res, next) {
-    res.json(data.getAllStudents());
+    res.json(data.getAllPosts());
 }
 
 
