f18fbf86d0f417795628511d5aee6b037950e975
damsu <damorizur@gmail.com>
2016-10-09 18:06:57 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -12,6 +12,34 @@ app.set('port', (process.env.PORT || 80));
 // bodyParser needs to be configured for parsing JSON from HTTP body
 app.use(bodyParser.json());
 
+// dummy users, in real life these would be in a database or something
+var users = [{
+    id: "txgw35",
+    username: "damien",
+    password: "pwd"
+},
+    {
+        id: "xvj2f2",
+        username: "john",
+        password: "doe"
+    }
+];
+
+app.post('/login', function (req, res) {
+    console.log("test");
+    console.log(req.body);
+    var u = users.find(function (element) {
+        return (element.username === req.body.username) && (element.password === req.body.password);
+    });
+
+    if (u !== undefined) {
+        return res.json({ id: u.id, username: u.username });
+    }
+    else {
+        return res.sendStatus(401);
+    }
+})
+
 // Mount our routes behind /api/ prefix
 //app.use('/api', books);
 //app.use('/api', authors);
